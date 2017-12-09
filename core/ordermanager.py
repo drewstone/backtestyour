67adba5cf8d60a13d0e3f10182c1726fab0c05fb
Drew Stone <drewstone329@gmail.com>
2017-12-08 22:49:44 -0500

    Refactoring polo subscriber

--- core/ordermanager.py
+++ core/ordermanager.py
@@ -2,44 +2,44 @@ import numpy as np
 from .orderbook import OrderBook
 
 class Order(object):
-	"""A simple order class"""
-	def __init__(self, type, price, quantity):
-		super(Order, self).__init__()
-		self.type = type
-		self.price = price
-		self.quantity = quantity
-	
+    """A simple order class"""
+    def __init__(self, type, price, quantity):
+        super(Order, self).__init__()
+        self.type = type
+        self.price = price
+        self.quantity = quantity
+    
 class OrderManager(object):
-	"""docstring for Orderbook"""
-	def __init__(self):
-		super(OrderManager, self).__init__()
-		self.lob = OrderBook()
+    """docstring for Orderbook"""
+    def __init__(self):
+        super(OrderManager, self).__init__()
+        self.lob = OrderBook()
 
-	def get_quote(self, order_type, trading_id, price, quantity):
-		sign = np.sign(quantity)
+    def get_quote(self, order_type, trading_id, price, quantity):
+        sign = np.sign(quantity)
 
-		quote = {
-			'type': order_type,
-			'trading_id': trading_id,
-			'price': price,
-			'qty': sign * quantity,
-			'tid': self.lob.time
-		}
+        quote = {
+            'type': order_type,
+            'trading_id': trading_id,
+            'price': price,
+            'qty': sign * quantity,
+            'tid': self.lob.time
+        }
 
-		if sign < 0:
-			quote['side'] = 'ask'
-		else:
-			quote['side'] = 'bid'
+        if sign < 0:
+            quote['side'] = 'ask'
+        else:
+            quote['side'] = 'bid'
 
-		return quote
+        return quote
 
-	def get_info(self):
-		return (self.lob.time, self.lob.getBestBid(), self.lob.getBestAsk())
+    def get_info(self):
+        return (self.lob.time, self.lob.getBestBid(), self.lob.getBestAsk())
 
-	def get_lob(self):
-		return self.lob
+    def get_lob(self):
+        return self.lob
 
-	def add_order(self, order_type, trading_id, price, quantity):
-		quote = self.get_quote(order_type, trading_id, price, quantity)
-		self.lob.processOrder(quote, fromData=False, verbose=False)
-		return quote
+    def add_order(self, order_type, trading_id, price, quantity):
+        quote = self.get_quote(order_type, trading_id, price, quantity)
+        self.lob.processOrder(quote, fromData=False, verbose=False)
+        return quote
--- exchanges/poloniex_websocket.py
+++ exchanges/poloniex_websocket.py
@@ -21,6 +21,9 @@ TICKER_SUBSCRIBE = 1002
 TICKER_OUTPUT = 'TICKER UPDATE {}={}(last),{}(lowestAsk),{}(highestBid),{}(percentChange),{}(baseVolume),{}(quoteVolume),{}(isFrozen),{}(high24hr),{}(low24hr)'
 TRADE_OUTPUT = 'TRADE {}={}(tradeId),{}(bookSide),{}(price),{}(size),{}(timestamp)'
 
+ORDER_LOGGING_STR = 'ooo : type = {}, price = {}, size = {}, ticker_id = {}'
+
+
 poloniex_market_pairs = [
     "USDT_BTC", "USDT_ETH", "USDT_XRP", "USDT_XMR", "USDT_LTC", "USDT_ETC", "USDT_DASH", "USDT_ZEC",
     "ETH_ETC", "ETC_ZEC",
@@ -73,7 +76,6 @@ class PoloniexSubscriber(object):
     def get_prices(self):
         return list(map(lambda key: {key: self.ob[key].get_info()}, self.ob.keys()))
 
-
     async def _subscribe(self):
         async with websockets.connect(API_LINK) as websocket:
             # first subscribe to ticker channel and all tickers update channels
@@ -85,101 +87,123 @@ class PoloniexSubscriber(object):
             # now parse received data
             while True:
                 message = await websocket.recv()
+                self.handle_message(message)
 
-                data = json.loads(message, object_pairs_hook=OrderedDict)
+    def handle_orderbook_snapshot(self, asks, bids):
+        for price, size in [order for order in asks]:
+            order = ('limit', ticker_id, float(price), -float(size))
+            self.ob[ticker_id].add_order(*order)
+            print(ORDER_LOGGING_STR.format(*order))
 
-                if 'error' in data:
-                    raise Exception('error arrived message={}'.format(message))
+        # BIDS
+        for price, size in [order for order in bids]:
+            order = ('limit', ticker_id, float(price), float(size))
+            self.ob[ticker_id].add_order(*order)
+            print(ORDER_LOGGING_STR.format(*order))
 
-                if data[0] == 1010:
-                    # this mean heartbeat
-                    continue
-                if len(data) < 2:
-                    raise Exception(
-                        'Short message arrived message={}'.format(message))
-                if data[1] == 1:
-                    # this mean the subscription is success
-                    continue
-                if data[1] == 0:
-                    # this mean the subscription is failure
-                    raise Exception(
-                        'subscription failed message={}'.format(message))
-                if data[0] == TICKER_SUBSCRIBE:
-                    values = data[2]
-                    ticker_id_int = values[0]
-                    # last = values[1]
-                    # lowestAsk = values[2]
-                    # highestBid = values[3]
-                    # percentChange = values[4]
-                    # baseVolume = values[5]
-                    # quoteVolume = values[6]
-                    # isFrozen = values[7]
-                    # high24hr = values[8]
-                    # low24hr = values[9]
-                    ticker_id = self._tickers_id[ticker_id_int]
-                    out_list = [ticker_id] + values[1:]
-                    # print(TICKER_OUTPUT.format(*out_list))
-                else:
-                    ticker_id = self._tickers_id[data[0]]
-                    seq = data[1]
+        sys.stdout.flush()
 
-                    for update in data[2]:
-                        # this mean this is snapshot
-                        if update[0] == 'i':
-                            # UPDATE[1]['currencyPair'] is the ticker name
-                            self._last_seq_dic[ticker_id] = seq
-                            # ASKS
-                            for price, size in [order for order in update[1]['orderBook'][0].items()]:
-                                self.ob[ticker_id].add_order('limit', ticker_id, float(price), -float(size))
 
-                            # BIDS
-                            for price, size in [order for order in update[1]['orderBook'][1].items()]:
-                                self.ob[ticker_id].add_order('limit', ticker_id, float(price), float(size))
+    def handle_orderbook_update(self, side, ticker_id, price, size):
+        # this mean remove
+        if size == '0.00000000':
+            print('Remove {},side={},price={}'.format(
+                ticker_id, side, price))
+        # this mean add or change
+        else:
+            print('Add or change {},side={},price={},size={}'.format(
+                ticker_id, side, price, size))
 
-                            # printing just 5 levels(this book can be 3000 levels)
-                            # print('{} book:'.format(ticker_id))
-                            # print('asks((price,size)):')
-                            # for level in asks[0:5]:
-                            #     print('({},{})'.format(level[0], level[1]))
-                            # print('bids((price,size)):')
-                            # for level in bids[0:5]:
-                            #     print('({},{})'.format(level[0], level[1]))
-                        # this mean add or change or remove
-                        elif update[0] == 'o':
-                            if self._last_seq_dic[ticker_id] + 1 != seq:
-                                raise Exception('Problem with seq number prev_seq={},message={}'.format(
-                                    self._last_seq_dic[ticker_id], message))
+        # Add check if necessary to see if new order crosses spread (LIMIT OR MARKET)
 
-                            price = float(update[2])
-                            side = 'bid' if update[1] == 1 else 'ask'
-                            size = float(update[3])
-                            print(data)
-                            print(ticker_id, price, side, size)
+        if side == 'ask':
+            order = ('limit', ticker_id, price, -size)
+        else:
+            order = ('limit', ticker_id, price, size)
 
-                            # this mean remove
-                            # if size == '0.00000000':
-                            #     print('Remove {},side={},price={}'.format(
-                            #         ticker_id, side, price))
-                            # # this mean add or change
-                            # else:
-                            #     print('Add or change {},side={},price={},size={}'.format(
-                            #         ticker_id, side, price, size))
-                        # this mean trade
-                        elif update[0] == 't':
-                            if self._last_seq_dic[ticker_id] + 1 != seq:
-                                raise Exception('Problem with seq number prev_seq={},message={}'.format(
-                                    self._last_seq_dic[ticker_id], message))
-                            print(update)
-                            trade_id = update[1]
-                            book_side = 'bid' if update[2] == 1 else 'ask'
-                            price = update[3]
-                            size = update[4]
-                            timestamp = update[5]
-                            out_list = [ticker_id, trade_id,
-                                        book_side, price, size, timestamp]
-                            # print(TRADE_OUTPUT.format(*out_list))
+        self.ob[ticker_id].add_order(*order)
 
+        print(ORDER_LOGGING_STR.format(*order))
+        sys.stdout.flush()
+
+
+    def handle_message(self, message):
+        data = json.loads(message, object_pairs_hook=OrderedDict)
+
+        if 'error' in data:
+            raise Exception('error arrived message={}'.format(message))
+
+        if data[0] == 1010:
+            # this mean heartbeat
+            continue
+        if len(data) < 2:
+            raise Exception(
+                'Short message arrived message={}'.format(message))
+        if data[1] == 1:
+            # this mean the subscription is success
+            continue
+        if data[1] == 0:
+            # this mean the subscription is failure
+            raise Exception(
+                'subscription failed message={}'.format(message))
+        if data[0] == TICKER_SUBSCRIBE:
+            values = data[2]
+            ticker_id_int = values[0]
+            # last = values[1]
+            # lowestAsk = values[2]
+            # highestBid = values[3]
+            # percentChange = values[4]
+            # baseVolume = values[5]
+            # quoteVolume = values[6]
+            # isFrozen = values[7]
+            # high24hr = values[8]
+            # low24hr = values[9]
+            ticker_id = self._tickers_id[ticker_id_int]
+            out_list = [ticker_id] + values[1:]
+            # print(TICKER_OUTPUT.format(*out_list))
+        else:
+            ticker_id = self._tickers_id[data[0]]
+            seq = data[1]
+
+            for update in data[2]:
+                # this mean this is snapshot
+                if update[0] == 'i':
+                    # UPDATE[1]['currencyPair'] is the ticker name
                     self._last_seq_dic[ticker_id] = seq
+
+                    asks = update[1]['orderBook'][0].items()
+                    bids = update[1]['orderBook'][1].items()
+
+                    self.handle_orderbook_snapshot(asks, bids)
+
+                # this mean add or change or remove
+                elif update[0] == 'o':
+                    if self._last_seq_dic[ticker_id] + 1 != seq:
+                        raise Exception('Problem with seq number prev_seq={},message={}'.format(
+                            self._last_seq_dic[ticker_id], message))
+
+                    price = float(update[2])
+                    side = 'bid' if update[1] == 1 else 'ask'
+                    size = float(update[3])
+
+                    self.handle_orderbook_update(side, ticker_id, price, size)
+
+                # this mean trade
+                elif update[0] == 't':
+                    if self._last_seq_dic[ticker_id] + 1 != seq:
+                        raise Exception('Problem with seq number prev_seq={},message={}'.format(
+                            self._last_seq_dic[ticker_id], message))
+                    print(update)
+                    trade_id = update[1]
+                    book_side = 'bid' if update[2] == 1 else 'ask'
+                    price = update[3]
+                    size = update[4]
+                    timestamp = update[5]
+                    out_list = [ticker_id, trade_id,
+                                book_side, price, size, timestamp]
+                    # print(TRADE_OUTPUT.format(*out_list))
+
+            self._last_seq_dic[ticker_id] = seq
 
 
 if __name__ == '__main__':
